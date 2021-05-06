539dac16511b86ee51f12e2165e80dcfdb70f086
Crispim Luiz <crispimluiz@live.com>
2021-05-06 12:12:44 -0300

    Criação do backEnd e FrontEnd

---
+++ backend/src/test/java/com/crispim/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crispim.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
